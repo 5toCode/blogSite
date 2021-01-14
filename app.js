d73b444aea7c844ae738b2e382f9d165edc99bd2
mrobertsjr <70207997+mrobertsjr@users.noreply.github.com>
2021-01-14 06:38:06 -0500

    set express routing parameters and mongoDB

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-const posts = [];
+// const posts = [];
 
 const dburl = process.env.DATABASEURL || 'mongodb://localhost:27017/blogsiteDB'
 
@@ -63,15 +63,20 @@ app.get('/compose', function (req, res) {
   res.render('compose', {});
 });
 
-app.get('/posts/:postTitle', function (req, res) {
-  const requestedTitle = _.lowerCase(req.params.postTitle);
-  const found = posts.find(post => _.lowerCase(post.postTitle) === requestedTitle);
-  if (found) {
-    res.render('post', {
-      postTitle: found.postTitle,
-      postBody: found.postBody,
-    })
-  }
+app.get('/posts/:postId', function (req, res) {
+  // const requestedTitle = _.lowerCase(req.params.postTitle);
+  console.log(req.params.postId);
+  const requestedId = req.params.postId;
+  console.log(requestedId);
+  Post.findOne({_id: requestedId}, function(err, foundPost) {
+    if(!err) {
+      console.log(foundPost);
+      res.render('post', {
+        postTitle: foundPost.postTitle,
+        postBody: foundPost.postBody
+      })
+    }
+  })
 });
 
 app.post('/compose', function (req, res) {
